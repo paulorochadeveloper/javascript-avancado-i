fc0924acb56a1336b570410030f4012d1de5df16
Paulo Rocha <paulorochaweb@bitbucket.org>
2019-09-13 10:36:14 +0100

    #FINISH MODULO 4  DATAS E LISTA

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -7,30 +7,34 @@ class NegociacaoController{
         this._inputData = $("#data");
         this._inputQuantidade = $("#quantidade");
         this._inputValor = $("#valor");
+        this._listaNegociacoes = new ListaNegociacoes();
 
     }
 
     adiciona(event){
 
         event.preventDefault();
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._limpaFormulario();
 
-        console.log(typeof(this._inputData.value));
+    }
 
-        let data = new Date(...
-            this._inputData.value
-                .split('-')
-                .map((item, indice) => item - indice % 2)
-            );
+    _criaNegociacao(){
 
-        console.log(data);
-
-        let negociacao = new Negociacao(
-            data,
+        return new Negociacao(
+            DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value,
         );
-        
-        console.log(negociacao);
+    }
+
+    _limpaFormulario(){
+
+        this._inputData.value = '';
+        this._inputQuantidade.value = '1';
+        this._inputValor.value = '0.0';
+
+        this._inputData.focus();
 
     }
 
